042fc18addef4b3ac77859a236802e54aa0e2868
Evan Westbrook <44587988+ewest12@users.noreply.github.com>
2018-11-13 12:29:27 -0800

    Input box function for urn

--- synthesizer.py
+++ synthesizer.py
@@ -16,6 +16,7 @@ from song import Song
 from synth_stats import SynthStats
 from play_mode import PlayMode
 from piano_keys import PianoKey
+from input_box import InputBox
 
 def run_program():
 	# Initialize the program and create a screen object.
@@ -26,12 +27,12 @@ def run_program():
 		(ap_settings.screen_width, ap_settings.screen_height))
 	pygame.display.set_caption("Synthesizer (beta)")
 	screen.fill(ap_settings.bg_color)
-	#initialize URN
-	#urn = 'random'
-	urn = 'spotify:track:375DLhrFmnn8kSa6ZcKGdQ'
 	
-	#initialize the play mode
+	#initialize the stats
 	stats = SynthStats(ap_settings)
+	#initialize URN
+	stats.urn = 'random'
+	#urn = 'spotify:track:4JehYebiI9JE8sR8MisGVb'
 	
 	# Create the group of buttons
 	buttons = scf.create_buttons(ap_settings, screen, stats)
@@ -41,6 +42,13 @@ def run_program():
 	playmode.prep_play_mode()
 	playmode.prep_playing_output() 
 	
+	#initialize the input box
+	x = 20
+	y = stats.button_height_max * 5
+	w = 140
+	h = 40
+	input_box = InputBox(x, y, w, h)
+	
 	# Create the group of piano keys
 	piano_keys = scf.create_keys(ap_settings, screen, stats)
 	
@@ -48,15 +56,13 @@ def run_program():
 	nplayer = NotePlayer(ap_settings)
 	apf.createNotes(ap_settings, nplayer, stats)
 	
-
-	
 	#start the main loop for the program
 	while True:
 		#check for events
-		scf.check_events(ap_settings, screen, buttons, nplayer, urn, stats, playmode, piano_keys)
+		scf.check_events(ap_settings, screen, buttons, nplayer, stats, playmode, piano_keys, input_box)
 
 		#redraw the screen
-		scf.update_screen(ap_settings, screen, buttons, playmode, piano_keys)
+		scf.update_screen(ap_settings, screen, buttons, playmode, piano_keys, input_box)
 
 run_program()
 
