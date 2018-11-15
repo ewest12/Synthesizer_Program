d770d63cb9c27f83a078009dc9cf4ea7edecd741
Evan Westbrook <44587988+ewest12@users.noreply.github.com>
2018-11-14 17:03:00 -0800

    Clean up code

--- play_mode.py
+++ play_mode.py
@@ -9,7 +9,6 @@ class PlayMode():
 		self.screen = screen
 		self.play_mode_screen_rect = screen.get_rect()
 		self.ap_settings = ap_settings
-		#self.play_mode_name = "None"
 		self.stats = stats
 		
 		# Font settings for play mode information.
@@ -21,29 +20,16 @@ class PlayMode():
 		self.prep_playing_output()
 		self.show_play_info()
 		
-		print("Playmode: " + self.stats.play_mode)
-		print("Artist: " + self.stats.playing_artist)
-		print("Track: " + self.stats.playing_track)
-		
 	def prep_play_mode(self):
 		"""Turn the mode into a rendered image."""
 		play_mode_str = "Play Mode: " + self.stats.play_mode
-		self.play_mode_image = self.font.render(play_mode_str, True, self.text_color,
-			self.ap_settings.bg_color)
+		self.play_mode_image = self.font.render(play_mode_str, True, 
+			self.text_color, self.ap_settings.bg_color)
 			
 		# Display the play mode at the top left of the screen. under the buttons
 		self.play_mode_rect = self.play_mode_image.get_rect()
-		print("Play mode rect")
-		print(self.play_mode_rect)
 		self.play_mode_rect.left = self.play_mode_screen_rect.left + 20
-		print("play mode screen rect left")
-		print(self.play_mode_screen_rect.left)
-		print("play mode rect")
-		print(self.play_mode_rect)
 		self.play_mode_rect.top = self.stats.button_height_max * 3
-		print("play mode rect")
-		print(self.play_mode_rect)
-		print("Playmode: " + self.stats.play_mode)
 		
 	def show_play_info(self):
 		"""Draw play mode to the  screen."""
@@ -55,16 +41,14 @@ class PlayMode():
 		self.playing_track = self.stats.playing_track
 		self.playing_artist = self.stats.playing_artist
 		playing_output_str = "Playing " + self.stats.playing_track + " by " + self.stats.playing_artist
-		self.playing_output_image = self.font.render(playing_output_str, True, self.text_color,
-			self.ap_settings.bg_color)
+		self.playing_output_image = self.font.render(playing_output_str, True, 
+			self.text_color, self.ap_settings.bg_color)
 			
 		# Display the artist info at at the top left screen. under play mode
 		self.playing_output_rect = self.playing_output_image.get_rect()
 		self.playing_output_rect.left = self.play_mode_screen_rect.left + 20
 		self.playing_output_rect.top = self.stats.button_height_max * 4
-		
-		print("Artist: " + self.stats.playing_artist)
-		print("Track: " + self.stats.playing_track)
+
 		
 
 	
